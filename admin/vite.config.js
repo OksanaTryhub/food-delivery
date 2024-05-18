7d8fe4a8ab8e0b9c30b9df9020e6d83ed56f4d6c
OksanaTryhub <ksanatrigub@gmail.com>
2024-05-18 13:09:08 +0300

    Updated admin/vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const base = import.meta.env.VITE_BASE_URL;
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base,
+  base: "/admin/",
   server: {
     proxy: {
       "/api": {

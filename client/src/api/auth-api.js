9e215b2752765f3e72e1742ee624aa4679c4d9b5
OksanaTryhub <108359208+OksanaTryhub@users.noreply.github.com>
2024-06-12 01:33:19 +0300

    Changed API

--- client/src/api/auth-api.js
+++ client/src/api/auth-api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  // baseURL: "https://food-delivery-mern-bhpe.onrender.com/api",
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://food-delivery-mern-bhpe.onrender.com/api",
+  // baseURL: "http://localhost:3000/api",
 });
 
 export const setTokenUser = (token) => {
